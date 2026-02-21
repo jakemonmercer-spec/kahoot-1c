6eae7690e27f31f1f62392c542cdd0f9f5bb9237
jakemonmercer-spec <jakemonmercer@gmail.com>
2026-02-21 20:44:48 +0600

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Конфигурация Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBrPKRi5NWA7JRcdbA2YLO4HPR3ULaBlGk",
+    authDomain: "quiz1c.firebaseapp.com",
+    databaseURL: "https://quiz1c-default-rtdb.firebaseio.com",
+    projectId: "quiz1c",
+    storageBucket: "quiz1c.firebasestorage.app",
+    messagingSenderId: "660786627580",
+    appId: "1:660786627580:web:6a552159559d0eef426879"
+};
+
+// Инициализация
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
